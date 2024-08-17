e33d66811ec541d5903698259006186ae4514d0e
Raman Markau <romanmarkov039@gmail.com>
2024-08-18 00:09:07 +0200

    terrafrom

---
+++ RestAPI_Lambda_AWS/lambda-func.py
@@ -0,0 +1,19 @@
+import boto3
+import os
+import datetime
+
+OWNER_NAME  = os.environ['OWNER_NAME']
+
+
+def lambda_handler(event, context):
+    try:
+        message = {
+            "owner"   : OWNER_NAME,
+            "message" : "This is AWS APIGateway + AWS Lambda",
+            "runtime" : str(datetime.datetime.now())
+            }
+
+    except Exception as error:
+        print("Error occuried! Error Message: " + str(error))
+
+    return message
